125eaa4321ac77a3a605f99c13d5982898d72767
surajmishra1007 <surajmishra1007@gmail.com>
2018-07-24 13:52:49 +0530

    Assignment 8.3

---
+++ main.js
@@ -0,0 +1,7 @@
+function printTimesTable(num) { //Javascript function
+    for (var i = 1; i <= 10; i++) { // For loop
+        console.log(num + " * " + i + " = " + num * i); //Output
+    }
+}
+
+printTimesTable(7); // function Calling
